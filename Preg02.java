4c08c355bc011d5cd1ada9331cc39f20f513f4b4
Yesenia Zuñiga <2113110050@untels.edu.pe>
2024-07-23 22:09:17 -0500

    Pregunta02 - Listo por Kim

---
+++ Preg02.java
@@ -0,0 +1,13 @@
+public class Preg02 {
+    public static void main(String[] args) {
+        int n = 100;
+        int sum = 0;
+
+        // Calculating the sum of first n natural numbers
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sumatoria = " + sum);
+    }
+}
